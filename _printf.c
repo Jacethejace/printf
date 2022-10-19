c7fd594b759c8c9e5168ac6f498419e7d3c1458d
Jacethejace <110991030+Jacethejace@users.noreply.github.com>
2022-10-19 05:12:06 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,40 +1,66 @@
-# include "main.h"
-# include <stdarg.h>
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - prints formatted output
- * @fmt: pointer to format string
- *
- * Return: number of character printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
-int _printf(const char *fmt, ...)
+int _printf(const char *format, ...)
 {
-	va_list ap;
-	int i;
-	unsigned long int cnt;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(ap, fmt);
-	i = 0;
-	cnt = 0;
-	if (!fmt || (*(fmt + i) == '%' && !*(fmt + i + 1)))
+	if (format == NULL)
 		return (-1);
-	if (!(*(fmt + i)))
-		return (0);
-	while (*(fmt + i))
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*(fmt + i) == '%')
+		if (format[i] != '%')
 		{
-			if (*(fmt + i + 1) == '%' || *(fmt + i - 1) == '\\')
-				cnt += _putchar(*(fmt + ++i));
-			else if (get_fmt_fun(fmt + i + 1))
-				cnt += get_fmt_fun((fmt + ++i))(ap);
-			else
-				cnt += _putchar(*(fmt + i));
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
-			cnt += _putchar(*(fmt + i));
-		i++;
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-	return (cnt);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
